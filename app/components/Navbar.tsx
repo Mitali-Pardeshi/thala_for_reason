ba07693fd4c8ec4e937f868ff890c71519b9f4d5
Rishi-Pardeshi <pardeshimitali757@gmail.com>
2024-06-13 15:01:58 +0530

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import {Github} from "lucide-react";
+import { Github } from "lucide-react";
 import ShareComp from "./Share";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
@@ -6,8 +6,12 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="w-full items-center justify-end flex p-6 gap-x-3">
-      <Link href={'https://github.com/rishi-boi/thala_for_reason'}><Button size={'icon'} variant={'outline'}><Github/></Button></Link>
-      <ShareComp website='thala-for-reason.vercel.app'/>
+      <Link href={"https://github.com/Mitali-Pardeshi"}>
+        <Button size={"icon"} variant={"outline"}>
+          <Github />
+        </Button>
+      </Link>
+      <ShareComp website="thala-for-reason.vercel.app" />
     </div>
   );
 };
